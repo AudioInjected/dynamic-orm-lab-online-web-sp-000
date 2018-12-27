f5376fe8d513bd2f4d4fad636065e642f47e7914
Rafael Batista <rafaelb24@yahoo.com>
2018-12-27 21:49:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,4 +21,9 @@ class InteractiveRecord
   def table_name_for_insert 
     self.class.table_name
   end
+  
+  def column_names_for_insert 
+    self.class.column_names
+   # binding.pry
+  end 
 end
